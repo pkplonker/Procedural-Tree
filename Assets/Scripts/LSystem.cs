50b08f8451957145af63b6537912079c1074061c
stuart <pkplonker@gmail.com>
2022-06-02 21:29:48 +0100

    Single Mesh generation started

--- Assets/Scripts/LSystem.cs
+++ Assets/Scripts/LSystem.cs
@@ -1,31 +1,29 @@
-using System;
-using System.CodeDom.Compiler;
-using System.Collections;
 using System.Collections.Generic;
 using System.Text;
+using Unity.Mathematics;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class LSystem : MonoBehaviour
 {
-	[SerializeField] private float rotationAngleMin = 0f;
 	[SerializeField] private float rotationAngleMax = 30f;
-	[SerializeField] private float upRotationAngleMin = -50f;
-	[SerializeField] private float upRotationAngleMax = 50f;
+
 	[SerializeField] private int iterations = 5;
 	[Range(0.01f, 1f)] [SerializeField] private float radius = 0.1f;
 	float branchLength;
 	[Range(0.01f, .5f)] [SerializeField] private float sliceThickness = .1f;
-	int numberOfSlices = 4;
-	[Range(1, 100)] [SerializeField] int quality = 10;
+	[Range(5, 100)] [SerializeField] int quality = 10;
 	[SerializeField] float radiusReductionFactor = 5f;
 
-	[SerializeField] private GameObject branchPrefab;
 	private const string axiom = "X";
 	private Stack<TransformInfo> transformStack;
 	private Dictionary<char, string> rules;
 	private string currentString = "";
-	private List<Mesh> meshes = new List<Mesh>();
+
+	public List<Vector3> vertices = new List<Vector3>();
+	private List<int> triangles = new List<int>();
+	private Transform targetTransform;
+	private int numberOfSlicesGenerated;
 
 	private void Start()
 	{
@@ -33,8 +31,9 @@ public class LSystem : MonoBehaviour
 		transformStack = new Stack<TransformInfo>();
 		rules = new Dictionary<char, string>
 		{
-//			{'X', "F&[[X]^X]^F[^FX]&X"},
-			{'X', "F/-[[X]+/X]+F[+?FX]-X?F/-[[X]+X]+F[+?FX]+"},
+			{'X', "F&[[X]^X]^F[^FX]&X"},
+			//{'X', "F/-[[X]+/X]+F[+?FX]-X?F/-[[X]+X]+F[+?FX]+"},
+			//{'X', "F"},
 
 			//{'F', "FF+[+F-F-F]-[-F+F+F]"},
 			//{'X', "[F-[+X]F[-X]+X]"},
@@ -44,20 +43,17 @@ public class LSystem : MonoBehaviour
 			//{'F', "F[+F]F[-F][F]"},
 			//{'X', "F"},
 			{'F', "FF"}
-			//{'A', "[&FLA]/////[&FL!A]///////[&FLA]"},
-			//{'F', "S ///// F"},
-			//{'S', "FL"},
-			//{'L',"[∧∧{-f+f+f--f+f+f}]"}
-			//{'L', "[∧∧]"}
-			/*
-			case '&':transform.Rotate(Vector3.left * Random.Range(0, rotationAngleMax));
-			case '^':transform.Rotate(Vector3.right * Random.Range(0, rotationAngleMax));
-			case '/':transform.Rotate(Vector3.up * Random.Range(0, rotationAngleMax));
-			case '?':transform.Rotate(Vector3.down * Random.Range(0, rotationAngleMax));
-			*/
 		};
+		//{'A', "[&FLA]/////[&FL!A]///////[&FLA]"},
+		//{'F', "S ///// F"},
+		//{'S', "FL"},
+		//{'L',"[∧∧{-f+f+f--f+f+f}]"}
+		//{'L', "[∧∧]"}
+
 		Generate();
+		GenerateFinalMesh();
 	}
+
 
 	private void Generate()
 	{
@@ -75,30 +71,20 @@ public class LSystem : MonoBehaviour
 			currentString = sb.ToString();
 		}
 
-		float baseRadius = radius;
-		float tipRadius = radius;
+		targetTransform = new GameObject("target").transform;
+		//generate start
+		GenerateSection();
+		GenerateSection();
 		for (int i = 0; i < currentString.Length; i++)
 		{
-			Vector3 initialPosition;
-			GameObject segment;
-			BranchGeneratorLSystem branch;
 			switch (currentString[i])
 			{
 				case 'F':
-					if (Random.value < 0.7f) break;
 
 					//straight line
-					baseRadius = tipRadius;
-					tipRadius = baseRadius - ((baseRadius / 100) * (radiusReductionFactor));
-
-					initialPosition = transform.position;
-					transform.Translate(Vector3.up * branchLength);
-					segment = Instantiate(branchPrefab);
-					branch = segment.GetComponent<BranchGeneratorLSystem>();
-					segment.transform.position = initialPosition;
-
-					meshes.Add(branch.GenerateBranchMesh(baseRadius, currentString[i] == ']' ? 0 : tipRadius,
-						sliceThickness, 3 + quality, numberOfSlices, true, transform.rotation));
+					targetTransform.Translate(Vector3.up * branchLength);
+					GenerateSection();
+					radius = radius - ((radius / 100) * (radiusReductionFactor));
 
 					break;
 				case 'f':
@@ -113,34 +99,33 @@ public class LSystem : MonoBehaviour
 				case 'L': //nothing
 					break;
 				case '+':
-					transform.Rotate(Vector3.back * rotationAngleMax);
+					targetTransform.Rotate(Vector3.back * rotationAngleMax);
 					break;
 				case '-':
-					transform.Rotate(Vector3.forward *  rotationAngleMax);
+					targetTransform.Rotate(Vector3.forward * rotationAngleMax);
 					break;
 				case '&':
-					transform.Rotate(Vector3.left *  rotationAngleMax);
+					targetTransform.Rotate(Vector3.left * rotationAngleMax);
 					break;
 				case '^':
-					transform.Rotate(Vector3.right *  rotationAngleMax);
+					targetTransform.Rotate(Vector3.right * rotationAngleMax);
 					break;
 				case '/':
-					transform.Rotate(Vector3.up * rotationAngleMax);
+					targetTransform.Rotate(Vector3.up * rotationAngleMax);
 					break;
 				case '?':
-					transform.Rotate(Vector3.down *  rotationAngleMax);
+					targetTransform.Rotate(Vector3.down * rotationAngleMax);
 					break;
 
 
 				case '[': //save
-					transformStack.Push(new TransformInfo(transform, baseRadius, tipRadius));
+					transformStack.Push(new TransformInfo(transform, radius));
 					break;
 				case ']': //return
 					TransformInfo ti = transformStack.Pop();
 					transform.position = ti.position;
 					transform.rotation = ti.rotation;
-					baseRadius = ti.baseRadius;
-					tipRadius = ti.tipRadius;
+					radius = ti.radius;
 					break;
 				default:
 					Debug.LogError("Error in string" + currentString[i]);
@@ -148,21 +133,105 @@ public class LSystem : MonoBehaviour
 			}
 		}
 	}
-}
 
-
-public class TransformInfo
-{
-	public Vector3 position;
-	public Quaternion rotation;
-	public float baseRadius;
-	public float tipRadius;
-
-	public TransformInfo(Transform t, float baseRadius, float tipRadius)
+	private void GenerateFinalMesh()
 	{
-		position = t.position;
-		rotation = t.rotation;
-		this.tipRadius = tipRadius;
-		this.baseRadius = baseRadius;
+		Mesh mesh = new Mesh
+		{
+			name = "New tree"
+		};
+		mesh.Clear();
+		mesh.SetVertices(vertices);
+		mesh.SetTriangles(triangles, 0);
+		mesh.RecalculateNormals();
+		MeshFilter mf = GetComponent<MeshFilter>();
+		mf.mesh = mesh;
+		transform.position = Vector3.zero;
+		transform.rotation = quaternion.identity;
+	}
+
+	private void GenerateSection()
+	{
+		GenerateVerts();
+
+		GenerateTriangles();
+	}
+
+	private void GenerateVerts()
+
+	{
+		numberOfSlicesGenerated++;
+		for (int y = 0; y < quality; y++)
+		{
+			vertices.Add(CalculateVertPosition(y));
+		}
+	}
+
+	private Vector3 CalculateVertPosition(int vertIndexAroundCircumference
+	)
+	{
+		float angleRadians = vertIndexAroundCircumference / (float) quality *
+		                     MathFunctions.TAU;
+
+
+		return targetTransform.rotation * new Vector3(
+			Mathf.Cos(angleRadians) * radius,
+			targetTransform.position.y,
+			Mathf.Sin(angleRadians) * radius
+		);
+	}
+
+	private void GenerateTriangles()
+	{
+		for (int i = 0; i < numberOfSlicesGenerated - 1; i++)
+		{
+			GenerateLayerTriangles(i);
+		}
+	}
+
+	private void GenerateLayerTriangles(int layer)
+	{
+		int layerAddition = layer * quality;
+		for (int i = 0; i < quality - 1; i++)
+		{
+			var root = i + layerAddition;
+			var rootLeft = root + 1;
+			int rootUpleft = root + quality + 1;
+			int rootUp = root + quality;
+
+
+			triangles.Add(root);
+			triangles.Add(rootUpleft);
+			triangles.Add(rootLeft);
+			triangles.Add(root);
+			triangles.Add(rootUp);
+			triangles.Add(rootUpleft);
+		}
+
+		int start = quality - 1 + layerAddition;
+		int startLeft = layerAddition;
+		int startLeftUp = start + 1;
+		int startup = start + quality;
+
+		triangles.Add(start);
+		triangles.Add(startLeftUp);
+		triangles.Add(startLeft);
+		triangles.Add(start);
+		triangles.Add(startup);
+		triangles.Add(startLeftUp);
+	}
+
+	private class TransformInfo
+	{
+		public Vector3 position;
+		public Quaternion rotation;
+		public float radius;
+
+		public TransformInfo(Transform t, float radius)
+		{
+			position = t.position;
+			rotation = t.rotation;
+			this.radius = radius;
+		}
 	}
 }
